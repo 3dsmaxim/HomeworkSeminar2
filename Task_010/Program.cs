69ffc15d7dc86ff3975cbb96133aaa8b2bf9de86
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-20 01:30:45 +0300

    Поправил 10-тое задание

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
-Console.WriteLine($"Ввведите трех значное число от 100 до 999: ");
+Console.WriteLine($"Ввведите трех значное целое число: ");
 
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
 if (number < 1000 && number > 99) Console.WriteLine($"Второе число {((number - (number % 10))% 100)/10}");
 
---
+++ Task_013/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine($"Ввведите трех значное целое число: ");
+
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (number < 1000 && number > 99) Console.WriteLine($"Второе число {((number - (number % 10))% 100)/10}");
