d736f81798945b0edbee68359d7f581971437afd
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-20 01:24:14 +0300

    Завершена 10-тая задача

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine($"Ввведите трех значное число от 100 до 999: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 99) Console.WriteLine($"Второе число {((number - (number % 10))% 100)/10}");
+
