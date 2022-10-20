56e870abac1914aedcefe7fc601da464f8a2c179
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-20 03:54:49 +0300

    Сделал 10-тое задание

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -5,5 +5,5 @@ Console.WriteLine($"Ввведите трех значное целое числ
 
 int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-if (number < 1000 && number > 99) Console.WriteLine($"Второе число {((number - (number % 10))% 100)/10}");
+if (number < 1000 && number > 99) Console.WriteLine($"Вторая цифра {((number - (number % 10))% 100)/10}");
 
--- Task_013/Program.cs
+++ Task_013/Program.cs
@@ -1,8 +1,44 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
-Console.WriteLine($"Ввведите трех значное целое число: ");
+
+
+int CountNumber(int number)
+
+
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = (number - (number % 10)) / 10;
+        count++;
+    }
+    return count;
+
+}
+
+Console.WriteLine($"Ввведите  целое число: ");
 
 int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-if (number < 1000 && number > 99) Console.WriteLine($"Второе число {((number - (number % 10))% 100)/10}");
+int count = 3;
+int countMax = CountNumber(number);
+
+if (countMax > 2)
+{
+    while (count < countMax)
+    {
+        number = (number - (number % 10)) / 10;
+
+        count = count + 1;
+    }
+
+    Console.WriteLine($"Третья цифра: {number % 10}.");
+    
+}
+
+else Console.WriteLine($"Третьей цифры нет.");
+
+
+
+
